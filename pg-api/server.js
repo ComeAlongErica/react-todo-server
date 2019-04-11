2c27009859847671f7b27a30e12f0d43b19b6356
ComeAlongErica <ericaraephoto@gmail.com>
2019-04-11 15:27:06 -0400

    pool connect is posting woohoo

--- pg-api/server.js
+++ pg-api/server.js
@@ -6,37 +6,37 @@ const app = express()
 const PORT = 3000
 
 const pool = pg.Pool({
-    user: 'postgres',
-    password: 'password',
-    host: 'localhost',
-    port: 5432,
-    database: 'postgres',
-    ssl: false,
-    max: 10
+  user: 'postgres',
+  password: 'password',
+  host: 'localhost',
+  port: 5432,
+  database: 'postgres',
+  ssl: false,
+  max: 10
 })
 
-pool.connect((error, db, done) => {
-    if (error) {
-        return console.log(error)
-    } else {
-        db.query('SELECT * FROM todo', (error, table) => {
-            if (error) {
-                return console.log(error)
-            } else {
-                console.log(table)
-            }
-        })
-    }
-})
+// pool.connect((error, db, done) => {
+//   if (error) {
+//     return console.log(error)
+//   } else {
+//     db.query('SELECT * FROM todo', (error, table) => {
+//       if (error) {
+//         return console.log(error)
+//       } else {
+//         console.log(table.rows)
+//       }
+//     })
+//   }
+// })
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(morgan('dev'))
 app.use(function (request, response, next) {
-    response.header('Access-Control-Allow-Origin', '*')
-    response.header('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Origin', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
 })
 
 app.listen(PORT, () => console.log('Listening on port: ' + PORT))
