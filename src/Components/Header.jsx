7ab547e9949db93ebce07004ed4523d29d94d3db
ComeAlongErica <ericaraephoto@gmail.com>
2019-04-12 19:57:27 -0400

    Add task component created

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import AddTask from './parts/AddTask'
+
 const HeaderContainer = styled.div`
 width: 100vw;
 background-image: linear-gradient( to right, #ffd74e, #ffae0d);
@@ -8,7 +10,9 @@ height: 120px;
 `
 
 const Header = props => {
-  return <HeaderContainer>Hello</HeaderContainer>
+  return <HeaderContainer>
+    <AddTask />
+  </HeaderContainer>
 }
 
 export default Header
---
+++ src/Components/parts/AddTask.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AddTaskContainer = styled.div`
+height: 30px;
+width: 30px;
+background-color: white;
+border-radius: 100%;
+`
+
+const AddTask = props => {
+  return <AddTaskContainer>
++
+  </AddTaskContainer>
+}
+
+export default AddTask
